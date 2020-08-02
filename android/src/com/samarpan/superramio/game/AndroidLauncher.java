987974b4b96ddb2e9676d526bb12305cdf1112cf
samarpan das <mailme.samarpandas@gmail.com>
2020-08-02 19:27:49 +0530

    First Commit : Initial GDX Setup Completed

---
+++ android/src/com/samarpan/superramio/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.samarpan.superramio.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.samarpan.superramio.game.SuperRamio;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SuperRamio(), config);
+	}
+}
