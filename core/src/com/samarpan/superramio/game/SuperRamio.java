c65b9e07e1008d161e4570c05dd3cdba3d2e132c
samarpan das <mailme.samarpandas@gmail.com>
2020-08-07 01:23:28 +0530

    Successfully updated gameState and Score.

--- core/src/com/samarpan/superramio/game/SuperRamio.java
+++ core/src/com/samarpan/superramio/game/SuperRamio.java
@@ -2,8 +2,10 @@ package com.samarpan.superramio.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
@@ -27,6 +29,11 @@ public class SuperRamio extends ApplicationAdapter
     float velocity = 0;
     int manY = 0;
     Rectangle manRectangle;
+    BitmapFont font;
+    Texture dizzy;
+
+    int score = 0;
+    int gameState = 0;
 
 
     ArrayList<Integer> coinXs = new ArrayList<Integer>();
@@ -70,6 +77,13 @@ public class SuperRamio extends ApplicationAdapter
         bomb = new Texture("bomb.png");
         random = new Random();
 
+        dizzy = new Texture("dizzy-1.png");
+
+
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(10);
+
 
 	}
 
@@ -106,99 +120,146 @@ public class SuperRamio extends ApplicationAdapter
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());  //showing background
                 // 0, 0 is the starting pos of the getWidth and Height will fill the entire height and width
 
-
-
-     ////////  start of coin part
-
-        if(coinCount < 100)                                          // coin frequency controller
+        if(gameState == 1)
         {
-            coinCount++;
-        }else {
-            coinCount = 0;
-            makeCoin();
-        }
 
-        coinRectangles.clear();
-        for(int i = 0; i < coinXs.size(); i++)
-        {
-            batch.draw(coin, coinXs.get(i), coinYs.get(i));
-            coinXs.set(i, coinXs.get(i) - 4);                     // the constant value is the speed controller
-
-            coinRectangles.add(new Rectangle(coinXs.get(i), coinYs.get(i), coin.getWidth(), coin.getHeight()));
-
-        }
-
-    ///// end of coin part
+            // GAME IS LIVE !!!
 
 
-    /////// start of bomb part
-        if(bombCount < 450)                                     //  bomb frequency controller
-        {
-            bombCount++;
-        }else {
-            bombCount = 0;
-            makeBomb();
-        }
+            ////////  start of coin part
 
-
-        bombRectangles.clear();
-        for(int i = 0; i < bombXs.size(); i++)
-        {
-            batch.draw(bomb, bombXs.get(i), bombYs.get(i));
-            bombXs.set(i, bombXs.get(i) - 7);                    // the constant value is the speed controller
-
-            bombRectangles.add(new Rectangle(bombXs.get(i), bombYs.get(i), bomb.getWidth(), bomb.getHeight()));
-
-        }
-
-    //////// end of bomb part
-
-
-
-
-
-    ////////    working with touch
-
-        if(Gdx.input.justTouched())
-        {
-            velocity = -10;
-        }
-    //////////////
-
-
-
-
-
-
-
-
-
-        // The pause if else statement is adding to slow down the speed of running of our man
-        if(pause < 6) // now after 8 iterations the state of the man changes making his speed look slower
-        {
-            pause++;
-        }else {
-            pause=0;
-            if(manState < 3)  // man state is actually changing the image of of the man to be displayed
+            if(coinCount < 100)                                          // coin frequency controller
             {
-                manState++;
+                coinCount++;
             }else {
-                manState = 0;
+                coinCount = 0;
+                makeCoin();
+            }
+
+            coinRectangles.clear();
+            for(int i = 0; i < coinXs.size(); i++)
+            {
+                batch.draw(coin, coinXs.get(i), coinYs.get(i));
+                coinXs.set(i, coinXs.get(i) - 4);                     // the constant value is the speed controller
+
+                coinRectangles.add(new Rectangle(coinXs.get(i), coinYs.get(i), coin.getWidth(), coin.getHeight()));
+
+            }
+
+            ///// end of coin part
+
+
+            /////// start of bomb part
+            if(bombCount < 450)                                     //  bomb frequency controller
+            {
+                bombCount++;
+            }else {
+                bombCount = 0;
+                makeBomb();
+            }
+
+
+            bombRectangles.clear();
+            for(int i = 0; i < bombXs.size(); i++)
+            {
+                batch.draw(bomb, bombXs.get(i), bombYs.get(i));
+                bombXs.set(i, bombXs.get(i) - 7);                    // the constant value is the speed controller
+
+                bombRectangles.add(new Rectangle(bombXs.get(i), bombYs.get(i), bomb.getWidth(), bomb.getHeight()));
+
+            }
+
+            //////// end of bomb part
+
+
+
+            ////////    working with touch
+
+            if(Gdx.input.justTouched())
+            {
+                velocity = -10;
+            }
+            //////////////
+
+
+
+         // The pause if else statement is adding to slow down the speed of running of our man
+            if(pause < 6) // now after 8 iterations the state of the man changes making his speed look slower
+            {
+                pause++;
+            }else {
+                pause=0;
+                if(manState < 3)  // man state is actually changing the image of of the man to be displayed
+                {
+                    manState++;
+                }else {
+                    manState = 0;
+                }
+            }
+
+
+            velocity += gravity;
+            manY -= velocity;
+
+            if(manY <= 0)
+                manY = 0;
+
+        }                                           // END OF GAMESTATE = 1
+
+
+        else if(gameState == 0)
+        {
+            // WAITING TO START
+
+            if(Gdx.input.justTouched())  // game will start once screen is touched
+            {
+                gameState = 1;
             }
         }
 
+        else if(gameState == 2)
+        {
+            // GAME OVER
 
-        velocity += gravity;
-        manY -= velocity;
+            if(Gdx.input.justTouched())  // game will start once screen is touched
+            {
+                gameState = 1;
 
-        if(manY <= 0)
-            manY = 0;
+                manY = Gdx.graphics.getHeight()/2 ;
+                score = 0;
+                velocity = 0;
+                coinXs.clear();
+                coinYs.clear();
+                coinRectangles.clear();
+                coinCount = 0;
+
+                bombXs.clear();
+                bombYs.clear();
+                bombRectangles.clear();
+                bombCount = 0;
+            }
 
 
 
-        //batch.draw(man[0], Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/4);
-        batch.draw(man[manState], Gdx.graphics.getWidth()/2 - man[manState].getWidth()/2, manY);
-        // getting the man at the exact center pos
+        }
+
+
+
+
+
+
+
+        if(gameState == 2)
+        {
+            batch.draw(dizzy, Gdx.graphics.getWidth()/2 - man[manState].getWidth()/2, manY);
+        }else {
+            //batch.draw(man[0], Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/4);
+            batch.draw(man[manState], Gdx.graphics.getWidth()/2 - man[manState].getWidth()/2, manY);
+            // getting the man at the exact center pos
+        }
+
+
+
 
         manRectangle = new Rectangle(Gdx.graphics.getWidth()/2 - man[manState].getWidth()/2, manY, man[manState].getWidth(), man[manState].getHeight());
 
@@ -208,6 +269,12 @@ public class SuperRamio extends ApplicationAdapter
             {
                 Gdx.app.log("Coin ::::", "Collision !!!");
                 Gdx.app.log("Index ::", Integer.toString(i));
+
+                score++;
+                coinRectangles.remove(i);
+                coinXs.remove(i);
+                coinYs.remove(i);
+                break;
             }
         }
 
@@ -217,8 +284,16 @@ public class SuperRamio extends ApplicationAdapter
             {
                 Gdx.app.log("Bomb ::::", "Collision !!!");
                 Gdx.app.log("Index ::", Integer.toString(i));
+
+
+                gameState = 2;     // ending the game
+
+
             }
         }
+
+
+        font.draw(batch, String.valueOf(score), 100, 200);
 
         batch.end();     // for ending the batch process
 
